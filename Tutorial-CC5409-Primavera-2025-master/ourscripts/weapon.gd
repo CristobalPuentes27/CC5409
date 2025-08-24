f19bd4fbbd10494770c3a7d030bc996ff7d79efe
Matías Neira <matias.neira.dev@gmail.com>
2025-08-24 18:45:54 -0400

    El arma se mueve al atacar

--- Tutorial-CC5409-Primavera-2025-master/ourscripts/weapon.gd
+++ Tutorial-CC5409-Primavera-2025-master/ourscripts/weapon.gd
@@ -4,7 +4,7 @@ extends Node2D
 var attacking := false
 var finalizing_attack := false
 var from := rotation
-var to = rotation + PI/2
+var to := rotation + PI/4
 
 func _physics_process(delta: float) -> void:
 	
@@ -12,14 +12,14 @@ func _physics_process(delta: float) -> void:
 		rotation = rotate_toward(rotation, to, .1)
 		print("r:", rotation)
 		print("to:", to)
-		if rotation == to:
+		if abs(rotation - to) < 0.01:
 			finalizing_attack = true
 	
 	if finalizing_attack:
 		rotation = rotate_toward(rotation, from, .1)
 		print(rotation)
 		print("asdfasdf")
-		if rotation == from:
+		if abs(rotation - from) < 0.01:
 			attacking = false
 			finalizing_attack = false
 
