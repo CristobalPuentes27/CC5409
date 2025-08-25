b22b223a184cd192a4b154f35e467eb59525c85d
Matías Neira <matias.neira.dev@gmail.com>
2025-08-24 21:03:30 -0400

    Se limpió el código

--- Tutorial-CC5409-Primavera-2025-master/ourscripts/weapon.gd
+++ Tutorial-CC5409-Primavera-2025-master/ourscripts/weapon.gd
@@ -10,15 +10,11 @@ func _physics_process(delta: float) -> void:
 	
 	if attacking and !finalizing_attack:
 		rotation = rotate_toward(rotation, to, .1)
-		print("r:", rotation)
-		print("to:", to)
 		if abs(rotation - to) < 0.01:
 			finalizing_attack = true
 	
 	if finalizing_attack:
 		rotation = rotate_toward(rotation, from, .1)
-		print(rotation)
-		print("asdfasdf")
 		if abs(rotation - from) < 0.01:
 			attacking = false
 			finalizing_attack = false
