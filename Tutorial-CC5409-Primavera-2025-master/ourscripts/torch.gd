a2417b1fe803f960a141bee488b2701ee0419b7f
cristobal207 <cristobal.puentes@ug.uchile.cl>
2025-12-05 14:22:36 -0300

    finals, map & new weapon

--- Tutorial-CC5409-Primavera-2025-master/ourscripts/torch.gd
+++ Tutorial-CC5409-Primavera-2025-master/ourscripts/torch.gd
@@ -2,11 +2,13 @@ extends Item
 
 @onready var point_light_2d: PointLight2D = $PointLight2D
 
+
 func _init() -> void:
 	spawnable=true
 	description = "Torch that will help you illuminate a bigger area"
 
 func _effect() -> void:
+	area_2d.monitoring=false
 	
 	point_light_2d.visible = true
 	
