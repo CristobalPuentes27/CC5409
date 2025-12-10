9ef5c349ee39d01c1e606b47ce4ce7ae8b246ab5
cristobal207 <cristobal.puentes@ug.uchile.cl>
2025-12-10 19:58:19 -0300

    add some sprite

--- Tutorial-CC5409-Primavera-2025-master/ourscripts/fire.gd
+++ Tutorial-CC5409-Primavera-2025-master/ourscripts/fire.gd
@@ -1,4 +1,5 @@
 extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const damage := 2
 
@@ -11,6 +12,7 @@ func _process(_delta: float) -> void:
 	if not is_multiplayer_authority(): return
 	if frames >= 500: rpc_queue_free.rpc()
 	frames += 1
+	animated_sprite_2d.play("default")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
